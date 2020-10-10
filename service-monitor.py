1f1268a48e39587933168bbc7330ca79c77494ee
Volodymyr Yatchenko <vyatchenko@luxoft.com>
2020-10-10 22:32:23 +0300

    os.kill() used to SIGSEGV

--- service-monitor.py
+++ service-monitor.py
@@ -4,40 +4,51 @@
 import os
 from time import sleep, time
 import sys
+import signal
 
 service_timeouts = {
-    'hmi-mainfu'            : 5,
-    'hmi-phonefu'           : 5,
-    'hmi-displaysettingsfu' : 5,
+    'hmi-mainfu.service'            : 4.5,
+    'hmi-phonefu.service'           : 4.5,
+    'hmi-displaysettingsfu.service' : 4.5,
 }
 
 class Service:
-    def __init__(self, servicename, timeout):
-        self.servicename=servicename
+    def __init__(self, name, timeout):
+        self.name=name
         self.timeout=timeout
+        self.uninit()
+
+    def uninit(self):
         self.activating_time = None
         self.coredumped = False
+        self.mainpid = None
 
-    def prop(self, prop):
-        return os.popen('systemctl --no-pager show {} --property {} --value'.format(self.servicename, prop)).read().rstrip()
+
+    def prop(self, propname):
+        return os.popen('systemctl --no-pager show {} --property {} --value'.format(self.name, propname)).read().rstrip()
 
     def check(self):
         state = self.prop('ActiveState')
-        if not self.activating_time and state == 'activating':
-            print('Activating service {}'.format(self.servicename))
-            self.activating_time = time()
-        elif self.activating_time and state == 'active':
-            print('Service {} is active'.format(self.servicename))
+        curtime = time()
+
+        if (self.activating_time is None) and state == 'activating':
+            print('Activating service {}'.format(self.name))
+            self.activating_time = curtime
+            self.mainpid = int(self.prop('MainPID'))
+        elif (self.activating_time is not None) and state == 'active':
+            print('Service {} is active'.format(self.name))
             self.activating_time = None
-        if state == 'activating' and time() - self.activating_time > self.timeout and not self.coredumped:
+        elif state == 'inactive' and self.coredumped:
+            print('Service {} is inactive'.format(self.name))
+            self.uninit()
+
+        if state == 'activating' and (curtime - self.activating_time > self.timeout) and not self.coredumped:
             self.coredumped = True
             self.dump()
 
     def dump(self):
-        print('Initiating {} coredump'.format(self.servicename))
-        os.system('touch /apps_data/dlt-core/{}.{}.gz.timeout'.format(self.servicename, self.prop('MainPID')))
-        os.system('systemctl kill -s SIGSEGV --kill-who=main {}'.format(self.servicename))
-
+        print('Initiating {} coredump'.format(self.name))
+        os.kill(self.mainpid, signal.SIGSEGV)
 
 services = [Service(s, t) for s, t in service_timeouts.items()]
 
@@ -45,4 +56,4 @@ while True:
     for s in services:
         s.check()
     sys.stdout.flush()
-    sleep(.5)
+    sleep(.1)
