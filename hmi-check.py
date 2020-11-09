3e59e754ff92bd7939da494f170805404ad6de12
Volodymyr Yatchenko <vyatchenko@luxoft.com>
2020-11-10 01:47:11 +0200

    +FU connection check

---
+++ hmi-check.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unbuffered
+import os
+import re
+from collections import namedtuple
+
+FU = namedtuple('FU',['fuclass','fuid','fil'])
+FUs = {
+    0 : FU("AudioManager"    , 101, "Main"            ),
+    1 : FU("AudioPlayer"     , 245, "Media"           ),
+    2 : FU("BTConnection"    , 153, "Main"            ),
+    3 : FU("Clock"           , 137, "Clock"           ),
+    4 : FU("Configuration"   ,   2, "Configuration"   ),
+    5 : FU("Lifecycle"       ,  66, "Lifecycle"       ),
+    6 : FU("Miracast"        ,  37, "Projection"      ),
+    7 : FU("Phone"           , 150, "Phone"           ),
+    8 : FU("Projection"      , 110, "Projection"      ),
+    9 : FU("SDLApplications" ,  12, "Projection"      ),
+    10: FU("Settings"        ,   1, "Main"            ),
+    11: FU("Tuner"           , 102, "Tuner"           ),
+    12: FU("VideoPlayer"     ,  13, "Media"           ),
+    13: FU("DisplaySettings" ,  21, "DisplaySettings" ), 
+    14: FU("ImagePlayer"     ,  14, "Media"           ),
+    15: FU("PhoneCalls"      , 152, "Phone"           ),
+    16: FU("QuickAccessHub"  ,   5, "Main"            ),
+    17: FU("RPAS"            , 111, "VehicleData"     ),
+    18: FU("Security"        ,  20, "Security"        ),
+    19: FU("VehicleData"     ,   3, "VehicleData"     ),
+    20: FU("VehicleVariant"  , 248, "VehicleData"     ),
+}
+
+ConnectedCommand = 'dlt-receive -a localhost|grep -e "fu::Lifecycle::doNotifyFUConnectedAction(U32) FU:"'
+ConnectedRegex = r".*fu::Lifecycle::doNotifyFUConnectedAction(U32) FU: (\d+).*"
+
+def check_fu_connected():
+    connected = set()
+    for s in os.popen(ConnectedCommand):
+        m = re.match(ConnectedRegex, s.rstrip())
+        if m:
+             connected.add(int(m.group(1)))
+
+    fils = set()
+    for i in FUs.keys():
+        if i not in connected:
+            fils.add(FUs[i].fil)
+
+    for fil in fils:
+        os.system('killall -s SIGSEGV HMI-{}FU'.format(fil))
--- service-monitor.py
+++ service-monitor.py
@@ -1,49 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import ctypes, os
+import os
 from time import sleep
+import unbuffered
+from monotonic import monotonic
 
 service_configs = [
     ('hmi-mainfu.service'  ,  7.0, ['pas-daemon.service']),
     #('hmi-phonefu.service' ,  4.9, ['pas-daemon.service']),
 ]
-
-class Unbuffered(object):
-   def __init__(self, stream):
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def writelines(self, datas):
-       self.stream.writelines(datas)
-       self.stream.flush()
-   def __getattr__(self, attr):
-       return getattr(self.stream, attr)
-
-import sys
-sys.stdout = Unbuffered(sys.stdout)
-
-## Monotonic time
-CLOCK_MONOTONIC_RAW = 4 # see <linux/time.h>
-
-class timespec(ctypes.Structure):
-    _fields_ = [
-        ('tv_sec', ctypes.c_long),
-        ('tv_nsec', ctypes.c_long)
-    ]
-
-librt = ctypes.CDLL('librt.so.1', use_errno=True)
-clock_gettime = librt.clock_gettime
-clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
-
-def monotonic():
-    t = timespec()
-    if clock_gettime(CLOCK_MONOTONIC_RAW , ctypes.pointer(t)) != 0:
-        errno_ = ctypes.get_errno()
-        raise OSError(errno_, os.strerror(errno_))
-    return t.tv_sec + t.tv_nsec * 1.0e-9
-##
 
 class Service:
     def __init__(self, name, timeout, add_dumps=None):
